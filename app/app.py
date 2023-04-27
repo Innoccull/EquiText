e557b5702aa5f72249b61335eba218b9336a75a4
Innoccull <christoph.rodgers@gmail.com>
2023-04-27 17:03:50 +1200

    WIP

---
+++ app/app.py
@@ -0,0 +1,10 @@
+# import libraries
+import dash
+import dash_bootstrap_components as dbc
+from dash import Input, Output, State, dcc, html
+
+import spacy
+from spacy import displacy
+import pandas as pd
+import json
+import os
